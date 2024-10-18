ff52dd1350cf10dbf8eb9000afa6f9d80a595bf8
fakamoto <facundogoiriz@gmail.com>
2024-10-18 08:04:50 -0300

    Refactor redeploy script to move built files to new directory accessible by Nginx

--- redeploy.sh
+++ redeploy.sh
@@ -6,6 +6,13 @@ git pull
 
 mkdocs build
 
+# Move the built files to the new directory accessible by Nginx
+sudo rm -rf /var/www/blog/*
+sudo cp -r site/* /var/www/blog/
+
+sudo chown -R www-data:www-data /var/www/blog
+sudo chmod -R 755 /var/www/blog
+
 sudo systemctl restart nginx
 
 echo "Blog redeployed successfully!"
